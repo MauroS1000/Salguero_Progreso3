2e2b479ebef87189bc391f25f5c780b33bf0b45e
Mauro Salguero <mauro.salguero@udla.edu.ec>
2024-01-24 08:52:15 -0500

    Funcionamiento terminado

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,24 +1,87 @@
-﻿namespace Salguero_Progreso3;
+﻿using Newtonsoft.Json;
+using Salguero_Progreso3.Model;
+
+namespace Salguero_Progreso3;
+
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    private int posicionRespuestaCorrecta;
+    public MainPage()
+    {
+        InitializeComponent();
+    }
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+        if (Connectivity.NetworkAccess == NetworkAccess.Internet)
+        {
+            using (var client = new HttpClient())
+            {
+                string url = $"https://opentdb.com/api.php?amount=10";
+                var response = await client.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    var json = await response.Content.ReadAsStringAsync();
+                    var rootObject = JsonConvert.DeserializeObject<Rootobject>(json);
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+                    if (rootObject.response_code == 0 && rootObject.results != null && rootObject.results.Length > 0)
+                    {
+                        var pregunta = rootObject.results[0];
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+                        
+                        var respuestas = pregunta.incorrect_answers.ToList();
+                        respuestas.Add(pregunta.correct_answer);
+                        respuestas = respuestas.OrderBy(x => Guid.NewGuid()).ToList();
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+                        Console.WriteLine(rootObject);
+                        questionlabelSalguero.Text = pregunta.question;
+
+                        
+                        posicionRespuestaCorrecta = respuestas.IndexOf(pregunta.correct_answer);
+
+                       
+                        answer1labelSalguero.Text = respuestas[0];
+                        answer2labelSalguero.Text = respuestas[1];
+                        answer3labelSalguero.Text = respuestas[2];
+                        answer4labelSalguero.Text = respuestas[3];
+                    }
+                }
+            }
+        }
+    }
+
+    private void CheckAnswer(int selectedAnswer)
+    {
+        if (posicionRespuestaCorrecta + 1 == selectedAnswer)
+        {
+            result.Text = "Perfect!";
+        }
+        else
+        {
+            result.Text = $"Oops, correct answer was {posicionRespuestaCorrecta + 1}";
+        }
+    }
+
+    private void Button_Clicked1(object sender, EventArgs e)
+    {
+        CheckAnswer(1);
+    }
+
+    private void Button_Clicked2(object sender, EventArgs e)
+    {
+        CheckAnswer(2);
+    }
+
+    private void Button_Clicked3(object sender, EventArgs e)
+    {
+        CheckAnswer(3);
+    }
+
+    private void Button_Clicked4(object sender, EventArgs e)
+    {
+        CheckAnswer(4);
+    }
 }
+
 
---
+++ Model/Question.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salguero_Progreso3.Model
+{
+
+    public class Rootobject
+    {
+        public int response_code { get; set; }
+        public Result[] results { get; set; }
+    }
+
+    public class Result
+    {
+        public string type { get; set; }
+        public string difficulty { get; set; }
+        public string category { get; set; }
+        public string question { get; set; }
+        public string correct_answer { get; set; }
+        public string[] incorrect_answers { get; set; }
+    }
+
+}
